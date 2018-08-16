15a5f64acb51b8f3a98228a5061e88c386cec3d7
李铭昕 <715557344@qq.com>
2018-08-16 21:12:21 +0800

    目录结果

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -5,4 +5,13 @@
  * @author   limx <limingxin@swoft.org>
  * @link     https://github.com/limingxinleo/swoft-component-creater
  */
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/config/define.php';
+
+// init the factory of bean
+\Swoft\Bean\BeanFactory::init();
+
+/* @var \Swoft\Bootstrap\Boots\Bootable $bootstrap */
+$bootstrap = \Swoft\App::getBean(\Swoft\Bootstrap\Bootstrap::class);
+$bootstrap->bootstrap();
---
+++ config/define.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  limingxin@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+!defined('DS') && define('DS', DIRECTORY_SEPARATOR);
+// App name
+!defined('APP_NAME') && define('APP_NAME', 'Swoft');
+// Project base path
+!defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
+
+// Register alias
+$aliases = [
+    '@root' => BASE_PATH,
+    '@env' => '@root',
+    '@src' => '@root/src',
+    '@runtime' => '@root/runtime',
+    '@configs' => '@root/config',
+    '@resources' => '@root/resources',
+    '@beans' => '@configs/beans',
+    '@properties' => '@configs/properties',
+    '@console' => '@beans/console.php',
+    '@vendor' => '@root/vendor',
+];
+
+\Swoft\App::setAliases($aliases);
