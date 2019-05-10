08faf047c2f69f432ec71ded54def74a676bb67e
李铭昕 <715557344@qq.com>
2019-05-10 17:02:14 +0800

    删除无用注释

--- template/small/tests/Cases/AbstractTestCase.php
+++ template/small/tests/Cases/AbstractTestCase.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * This file is part of Swoft.
- *
- * @link     https://swoft.org
- * @document https://doc.swoft.org
- * @contact  limingxin@swoft.org
- * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
- */
+
 namespace SwoftTest\Cases;
 
 use PHPUnit\Framework\TestCase;
--- template/small/tests/bootstrap.php
+++ template/small/tests/bootstrap.php
@@ -1,10 +1,3 @@
 <?php
-/**
- * This file is part of Swoft.
- *
- * @link     https://swoft.org
- * @document https://doc.swoft.org
- * @contact  limingxin@swoft.org
- * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
- */
+
 require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
